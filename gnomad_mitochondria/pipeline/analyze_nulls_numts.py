3248c5c63bf04c96299334cec2d4d83e7e9727e2
Rahul <rahulg603@gmail.com>
2022-03-29 14:19:03 -0400

    Further optimized exporting functions

--- gnomad_mitochondria/pipeline/analyze_nulls_numts.py
+++ gnomad_mitochondria/pipeline/analyze_nulls_numts.py
@@ -19,7 +19,7 @@ logger = logging.getLogger("Annotate coverage")
 logger.setLevel(logging.INFO)
 
 
-def multi_way_union_mts(mts: list, temp_dir: str, chunk_size: int, min_partitions: int) -> hl.MatrixTable:
+def multi_way_union_mts(mts: list, temp_dir: str, chunk_size: int) -> hl.MatrixTable:
     """
     Hierarchically join together MatrixTables in the provided list.
 
@@ -46,8 +46,6 @@ def multi_way_union_mts(mts: list, temp_dir: str, chunk_size: int, min_partition
 
             # Multiway zip join will produce an __entries annotation, which is an array where each element is a struct containing the __entries annotation (array of structs) for that sample
             merged = hl.Table.multi_way_zip_join(to_merge, "__entries", "__cols")
-            if min_partitions > 10:
-                merged = merged.checkpoint(f"stage_{stage}_job_{i}_pre.ht", overwrite=True)
             # Flatten __entries while taking into account different entry lengths at different samples/variants (samples lacking a variant will be NA)
             merged = merged.annotate(
                 __entries=hl.flatten(
@@ -94,7 +92,7 @@ def main(args):  # noqa: D103
     temp_dir = args.temp_dir
     chunk_size = args.chunk_size
     overwrite = args.overwrite
-    keep_targets = args.keep_targets
+    expect_shifted = args.expect_shifted
 
     if args.overwrite == False and hl.hadoop_exists(output_ht):
         logger.warning(
@@ -115,55 +113,67 @@ def main(args):  # noqa: D103
             line = line.rstrip()
             items = line.split("\t")
             participant_id, base_level_coverage_metrics, sample = items[0:3]
-            mt = hl.import_matrix_table(
+            typedict = {"chrom": hl.tstr, "pos": hl.tint, "target": hl.tstr, "coverage_original": hl.tint, "coverage_remapped_self": hl.tint}
+            if expect_shifted:
+                typedict.update({'coverage_remapped_self_shifted':hl.tint})
+            ht = hl.import_table(
                 base_level_coverage_metrics,
                 delimiter="\t",
-                row_fields={"chrom": hl.tstr, "pos": hl.tint, "target": hl.tstr},
-                row_key=["chrom", "pos"],
-                min_partitions=args.n_read_partitions,
-            )
-            if not keep_targets:
-                mt = mt.drop("target")
-            else:
-                mt = mt.key_rows_by(*["chrom", "pos", "target"])
-            mt = mt.rename({"x": "coverage"})
-            mt = mt.key_cols_by(s=sample)
+                types=typedict,
+                key=["chrom", "pos"],
+            ).drop("target")
+            mt1 = ht.select('coverage_original').to_matrix_table_row_major(columns = ['coverage_original'], entry_field_name = 'coverage_original', col_field_name='s')
+            mt2 = ht.select('coverage_remapped_self').to_matrix_table_row_major(columns = ['coverage_remapped_self'], entry_field_name = 'coverage_remapped_self', col_field_name='s')
+            mt1 = mt1.key_cols_by(s=sample)
+            mt2 = mt2.key_cols_by(s=sample)
+            mt = mt1.annotate_entries(coverage_remapped_self = mt2[mt1.row_key, mt1.col_key].coverage_remapped_self)
+            if expect_shifted:
+                mt3 = ht.select('coverage_remapped_self_shifted').to_matrix_table_row_major(columns = ['coverage_remapped_self_shifted'], entry_field_name = 'coverage_remapped_self_shifted', col_field_name='s')
+                mt3 = mt3.key_cols_by(s=sample)
+                mt = mt.annotate_entries(coverage_remapped_self_shifted = mt3[mt.row_key, mt.col_key].coverage_remapped_self_shifted)
             mt_list.append(mt)
 
     logger.info("Joining individual coverage mts...")
     out_dir = dirname(output_ht)
 
-    cov_mt = multi_way_union_mts(mt_list, temp_dir, chunk_size, min_partitions=args.n_read_partitions)
+    cov_mt = multi_way_union_mts(mt_list, temp_dir, chunk_size)
     n_samples = cov_mt.count_cols()
 
     logger.info("Adding coverage annotations...")
     # Calculate the mean and median coverage as well the fraction of samples above 100x or 1000x coverage at each base
     cov_mt = cov_mt.annotate_rows(
         locus=hl.locus(cov_mt.chrom, cov_mt.pos, reference_genome="GRCh38"),
-        mean=hl.float(hl.agg.mean(cov_mt.coverage)),
-        median=hl.median(hl.agg.collect(cov_mt.coverage)),
-        over_100=hl.float((hl.agg.count_where(cov_mt.coverage > 100) / n_samples)),
-        over_1000=hl.float((hl.agg.count_where(cov_mt.coverage > 1000) / n_samples)),
+        mean_original=hl.float(hl.agg.mean(cov_mt.coverage_original)),
+        mean_remapped=hl.float(hl.agg.mean(cov_mt.coverage_remapped_self)),
+        median_original=hl.median(hl.agg.collect(cov_mt.coverage_original)),
+        median_remapped=hl.median(hl.agg.collect(cov_mt.coverage_remapped_self))
     )
+    if expect_shifted:
+        cov_mt = cov_mt.annotate_rows(
+            mean_remapped_shifted=hl.float(hl.agg.mean(cov_mt.coverage_remapped_self_shifted)),
+            median_remapped_shifted=hl.median(hl.agg.collect(cov_mt.coverage_remapped_self_shifted))
+        )
     cov_mt.show()
 
     cov_mt = cov_mt.key_rows_by("locus").drop("chrom", "pos")
 
     output_mt = re.sub(r"\.ht$", ".mt", output_ht)
     output_tsv = re.sub(r"\.ht$", ".tsv", output_ht)
-    output_samples = re.sub(r"\.ht$", "_sample_level.txt", output_ht)
+    output_samples_orig = re.sub(r"\.ht$", "_original_sample_level.txt", output_ht)
+    output_samples_remap = re.sub(r"\.ht$", "_remapped_sample_level.txt", output_ht)
 
-    if not args.hail_only:
-        logger.info("Writing sample level coverage...")
-        sample_mt = cov_mt.key_rows_by(pos=cov_mt.locus.position)
-        sample_mt.coverage.export(output_samples)
+    logger.info("Writing sample level coverage...")
+    sample_mt = cov_mt.key_rows_by(pos=cov_mt.locus.position)
+    sample_mt.coverage_original.export(output_samples_orig)
+    sample_mt.coverage_remapped_self.export(output_samples_remap)
+    if expect_shifted:
+        sample_mt.coverage_remapped_self_shifted.export(re.sub(r"\.ht$", "_remapped_shifted_sample_level.txt", output_ht))
 
     logger.info("Writing coverage mt and ht...")
     cov_mt.write(output_mt, overwrite=overwrite)
     cov_ht = cov_mt.rows()
     cov_ht = cov_ht.checkpoint(output_ht, overwrite=overwrite)
-    if not args.hail_only:
-        cov_ht.export(output_tsv)
+    cov_ht.export(output_tsv)
 
 
 if __name__ == "__main__":
@@ -201,13 +211,7 @@ if __name__ == "__main__":
         "--overwrite", help="Overwrites existing files", action="store_true"
     )
     parser.add_argument(
-        "--keep-targets", help="Will add an annotation for target from the coverage file", action="store_true"
-    )
-    parser.add_argument(
-        "--n-read-partitions", type=int, help="The number of partitions to use when reading tsvs. This should be 1 if the files are small.", default=1
-    )
-    parser.add_argument(
-        "--hail-only", action='store_true', help='Skip generating flat files.'
+        "--expect-shifted", action="store_true"
     )
 
     args = parser.parse_args()
